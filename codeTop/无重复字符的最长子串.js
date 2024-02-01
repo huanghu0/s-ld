73ee67371236d5cef898819e3cd0775bf385de5c
huanghu0 <1525273035@qq.com>
2024-02-01 23:28:09 +0800

    无重复字符的最长子串

--- codeTop/无重复字符的最长子串.js
+++ codeTop/无重复字符的最长子串.js
@@ -5,14 +5,18 @@
  */
 var lengthOfLongestSubstring = function(s) {
     let map = new Map();
-    let max = 0,start = 0
+    let max = 0,start = 0;
+    // let maxStr = '';
     for(let end = 0;end < s.length;end++){
         let endChar = s[end]
         if(map.has(endChar)){
             start = Math.max(map.get(endChar) + 1,start) 
         }
         max = Math.max(max,end - start + 1)
+        // maxStr = maxStr.length > s.slice(start,end+1).length ? maxStr : s.slice(start,end+1)
         map.set(endChar,end)
     }
     return max
 };
+
+console.log(lengthOfLongestSubstring('abcabcbb'))
